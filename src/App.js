7399fa34ce5df0cb2c31a11468ef253bd4e60ca3
Dana <stockhoffd@gmail.com>
2019-06-08 11:13:24 -0700

    displaying planets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Goals from './components/Goals';
+//import Goals from './components/Goals';
+import Planets from './components/Planets';
 
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Goals uri="http://localhost:3001/staticGoals" />
+        <Planets uri="http://localhost:3001/staticPlanets" />
       </div>
     );
   }

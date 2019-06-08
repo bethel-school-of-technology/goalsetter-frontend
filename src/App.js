8330e54ddc111315f851037785f70711a9ace76d
Kevin Bjorklund <kbjorklund@Kevins-MBP.attlocal.net>
2019-06-08 13:47:54 -0500

    Adding Login Page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Goals from './components/Goals';
+import Login from './pages/Login';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
+        <Login />
         <Goals uri="http://localhost:3001/staticGoals" />
       </div>
     );

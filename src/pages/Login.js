8f02b1d216b841caa75c8d28c009ffc6e26bae63
Kevin Bjorklund <kbjorklund@Kevins-MBP.attlocal.net>
2019-06-08 21:47:02 -0500

    Added Bootstrap and Reactstrap

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+   Container, Col, Form,
+   FormGroup, Label, Input,
+   Button,
+ } from 'reactstrap';
 export default class Login extends Component { 
   state = { 
   }
@@ -6,7 +11,67 @@ export default class Login extends Component {
   render () {                                   
       return (
         <div>
-           <h1> connected! </h1>
+           <Container className="App">
+          <h2>Sign In | Sign Up</h2>
+        <Form id="signup" name="signup" method="POST" action="/">
+          <Col>
+            <FormGroup>
+              <Label>First Name:</Label>
+              <Input
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="First Name"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Last Name</Label>
+              <Input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="Last Name"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="myemail@email.com"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Username</Label>
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="username">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Button>Submit</Button>
+        </Form>
+        </Container>
         </div>
       )
    }

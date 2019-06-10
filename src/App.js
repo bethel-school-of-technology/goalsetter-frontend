f010a9f9ff0fc8168c350ea06a4bc8096104482f
Kevin Bjorklund <kbjorklund@Kevins-MBP.attlocal.net>
2019-06-10 18:35:41 -0500

    Reverted to old so we won't have merge conflicts.

--- src/App.js
+++ src/App.js
@@ -16,39 +16,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header> */}
-        <div className="App__Aside">
-          <div className="PageSwitcher">
-            <a href="#" className="PageSwitcher__Item">Sign In</a>
-            <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
-          </div>
-          <div className="FormTitle">
-            <a href="#" className="FormTitle__Link">Sign In</a> or <a href="#" className="FormTitle__Link FormTitle__Link--Active">Sign Up</a>
-          </div>
-
-          <div>
-            <div className="FormCenter">
-              <form className="FormFields" onSubmit={this.handleSubmit}>
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="Username">User Name</label>
-                  <input type="text" id="Username" className="FormField__Input" placeholder="Enter Your Username" name="Username" />
-                </div>
-              </form>
-            </div>
-          </div>
-          <div>
-            <div className="FormCenter">
-              <form className="FormFields" onSubmit={this.handleSubmit}>
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="password">Password</label>
-                  <input type="password" id="Password" className="FormField__Input" placeholder="Enter Your Password" name="Password" />
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        {/* <Login/>
+        <Login/>
         <Users uri="http://localhost:3001/allusers" />
-        <Planets uri="http://localhost:3001/staticPlanets" /> */}
+        <Planets uri="http://localhost:3001/staticPlanets" />
       </div>
     );
   }

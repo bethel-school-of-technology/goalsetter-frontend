34f4d7f206740ab95fe58b24155ceb55a1da9f47
Dana <stockhoffd@gmail.com>
2019-06-23 19:17:44 -0700

    onChange Event

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,36 @@ import {
  import Header from '../Components/Header';
 
 export default class Home extends Component {
-    state = { 
+    state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      errMessage: ''
     }
+
+    createUser = () => {
+      const { firstName, lastName, email, password } = this.state;
+      console.log('this.state: ', this.state)
+      // what is the current state of name, etc, make sure it's complete.
+      if (firstName === '') {
+        this.setState({
+          errMessage: 'First Name Required'
+        })
+      }
+      // Send data to db via axios post to create user, if successful then reroute user to your other client route that matches login
+    }    
+
+    onChange = event => {
+      console.log('Event: ', event)
+      console.log('Event name: ', event.target.name)
+      console.log('Event Value: ', event.target.value)
+      // update the state of that with the eent.target.value so that your state is the data
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+
     render () {                                   
        return (
          <Container className="App">
@@ -22,9 +50,10 @@ export default class Home extends Component {
             <Label>First Name:</Label>
             <Input
               type="text"
-              name="FirstName"
+              name="firstName"
               id="FirstName"
               placeholder="First Name"
+              onChange={this.onChange}
             />
           </FormGroup>
         </Col>
@@ -33,9 +62,10 @@ export default class Home extends Component {
             <Label>Last Name</Label>
             <Input
               type="text"
-              name="LastName"
+              name="lastName"
               id="LastName"
               placeholder="Last Name"
+              onChange={this.onChange}
             />
           </FormGroup>
         </Col>
@@ -44,9 +74,10 @@ export default class Home extends Component {
             <Label>Email</Label>
             <Input
               type="Email"
-              name="Email"
+              name="email"
               id="Email"
               placeholder="myemail@email.com"
+              onChange={this.onChange}
             />
           </FormGroup>
         </Col>
@@ -55,13 +86,14 @@ export default class Home extends Component {
             <Label for="email">Password</Label>
             <Input
               type="password"
-              name="Password"
+              name="password"
               id="Password"
               placeholder="********"
+              onChange={this.onChange}
             />
           </FormGroup>
         </Col>
-        <Button>Submit</Button>
+        <Button onClick={this.createUser}>Submit</Button>
       </Form>
       </Container>
        
